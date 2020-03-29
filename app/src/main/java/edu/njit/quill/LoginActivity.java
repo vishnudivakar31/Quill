f4df4fbf1b767b7b495bbe5a2734c1bf347f2b72
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-03-28 21:19:50 -0400

    create activity

--- app/src/main/java/edu/njit/quill/LoginActivity.java
+++ app/src/main/java/edu/njit/quill/LoginActivity.java
@@ -2,13 +2,28 @@ package edu.njit.quill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton, createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.emailSignInBtn);
+        createAccountButton = findViewById(R.id.loginCreateAccoutBtn);
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, CreateAccountActivity.class));
+            }
+        });
     }
 }
