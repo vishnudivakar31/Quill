7672d31b18941cb259887d2bf756cf1ef0713e75
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-03-28 20:43:04 -0400

    welcome page impl

--- app/src/main/java/edu/njit/quill/MainActivity.java
+++ app/src/main/java/edu/njit/quill/MainActivity.java
@@ -3,12 +3,15 @@ package edu.njit.quill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
     }
 }
