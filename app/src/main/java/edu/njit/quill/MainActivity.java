09c56c6d219ccb677ee9ba67970dd0839d3735f0
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-03-28 21:03:19 -0400

    login screen impl

--- app/src/main/java/edu/njit/quill/MainActivity.java
+++ app/src/main/java/edu/njit/quill/MainActivity.java
@@ -2,16 +2,28 @@ package edu.njit.quill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+
+        startBtn = findViewById(R.id.startButton);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
