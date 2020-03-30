985c66e7fa6c808be423da161eb2e1dc8ef8c404
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-03-30 01:20:59 -0400

    journal list and sign out

--- app/src/main/java/edu/njit/quill/JournalListActivity.java
+++ app/src/main/java/edu/njit/quill/JournalListActivity.java
@@ -2,13 +2,76 @@ package edu.njit.quill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
-public class JournalListActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
+
+public class JournalListActivity extends AppCompatActivity implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
+
+    private FloatingActionButton popUpButton;
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_list);
+
+        popUpButton = findViewById(R.id.popup_button);
+
+        popUpButton.setOnClickListener(this);
+
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.popup_button:
+                showPopupMenu();
+        }
+    }
+
+    private void showPopupMenu() {
+        PopupMenu popupMenu = new PopupMenu(this, popUpButton);
+        popupMenu.inflate(R.menu.menu);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.action_add:
+                if(firebaseUser != null && auth != null) {
+                    startActivity(new Intent(JournalListActivity.this, PostJournalActivity.class));
+                }
+                break;
+            case R.id.action_signout:
+                if(firebaseUser != null && auth != null) {
+                    auth.signOut();
+                    startActivity(new Intent(JournalListActivity.this, MainActivity.class));
+                }
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
     }
 }
--- app/src/main/java/edu/njit/quill/LoginActivity.java
+++ app/src/main/java/edu/njit/quill/LoginActivity.java
@@ -1,29 +1,58 @@
 package edu.njit.quill;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import edu.njit.quill.util.JournalApi;
+
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String USERS = "users";
     private Button loginButton, createAccountButton;
+    private EditText emailEditText, passwordEditText;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection(USERS);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        auth = FirebaseAuth.getInstance();
+
         loginButton = findViewById(R.id.emailSignInBtn);
         createAccountButton = findViewById(R.id.loginCreateAccoutBtn);
+        emailEditText = findViewById(R.id.login_email);
+        passwordEditText = findViewById(R.id.login_password);
+        progressBar = findViewById(R.id.login_progress);
 
         createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +60,63 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, CreateAccountActivity.class));
             }
         });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginEmailPassword(emailEditText.getText().toString().trim(), passwordEditText.getText().toString().trim());
+            }
+        });
+    }
+
+    private void loginEmailPassword(String email, String pwd) {
+        if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pwd)) {
+            progressBar.setVisibility(ProgressBar.VISIBLE);
+            auth.signInWithEmailAndPassword(email, pwd)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            FirebaseUser user = auth.getCurrentUser();
+                            final String currentUserId = user.getUid();
+
+                            collectionReference
+                                    .whereEqualTo("user_id", currentUserId)
+                                    .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots,
+                                                            @Nullable FirebaseFirestoreException e) {
+                                            if(e == null) {
+                                                assert queryDocumentSnapshots != null;
+                                                if(!queryDocumentSnapshots.isEmpty()) {
+                                                    for(QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                                        progressBar.setVisibility(ProgressBar.INVISIBLE);
+                                                        JournalApi journalApi = JournalApi.getInstance();
+                                                        journalApi.setUsername(snapshot.getString("username"));
+                                                        journalApi.setUserId(currentUserId);
+
+                                                        startActivity(new Intent(LoginActivity.this,
+                                                                PostJournalActivity.class));
+                                                        finish();
+                                                    }
+                                                }
+                                            } else {
+                                                progressBar.setVisibility(ProgressBar.INVISIBLE);
+                                                Snackbar.make(findViewById(R.id.login_activitiy), R.string.invalid_user, Snackbar.LENGTH_SHORT).show();
+                                                return;
+                                            }
+                                        }
+                                    });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressBar.setVisibility(ProgressBar.INVISIBLE);
+                            Snackbar.make(findViewById(R.id.login_activitiy), R.string.invalid_user, Snackbar.LENGTH_SHORT).show();
+                        }
+                    });
+        } else {
+            Snackbar.make(findViewById(R.id.login_activitiy), R.string.mandatory_warning, Snackbar.LENGTH_SHORT).show();
+        }
     }
 }
